c09449f89a5d0cd2f24b557e16cd7fc4229ee2a0
Zhi Kai Li <muggy8@gmail.com>
2023-01-04 06:52:37 -0800

    create project in android studio

---
+++ kotlin/app/src/main/java/com/muggy8/spell_check_writer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.muggy8.spell_check_writer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
