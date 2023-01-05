d677f9ca8ed7baabee0c2a96364544543de0a975
Zhi Kai Li <muggy8@gmail.com>
2023-01-05 11:22:10 -0800

    got the menu to work on the virtual device

--- kotlin/app/src/main/java/com/muggy8/spell_check_writer/MainActivity.kt
+++ kotlin/app/src/main/java/com/muggy8/spell_check_writer/MainActivity.kt
@@ -2,10 +2,14 @@ package com.muggy8.spell_check_writer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var primaryToolbar:Toolbar = findViewById(R.id.primary_toolbar)
+        setSupportActionBar(primaryToolbar)
     }
 }
