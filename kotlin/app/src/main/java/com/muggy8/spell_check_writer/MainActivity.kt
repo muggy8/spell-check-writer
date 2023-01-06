8802b48b6d14305196bb2d8eab9859bd1794cf1c
Muggy Ate <muggy8@users.noreply.github.com>
2023-01-06 08:16:41 -0800

    can now add stuff to the files list dynamically

--- kotlin/app/src/main/java/com/muggy8/spell_check_writer/MainActivity.kt
+++ kotlin/app/src/main/java/com/muggy8/spell_check_writer/MainActivity.kt
@@ -2,13 +2,16 @@ package com.muggy8.spell_check_writer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import java.util.Random
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var primaryToolbar:Toolbar
     private lateinit var filesDrawer:NavigationView
     private lateinit var filesListToggle:ActionBarDrawerToggle
@@ -33,6 +36,8 @@ class MainActivity : AppCompatActivity() {
 
         mainAppView.addDrawerListener(filesListToggle)
         filesListToggle.syncState()
+
+        buildFilesMenu(filesDrawer)
     }
 
     override fun onBackPressed() {
@@ -41,5 +46,60 @@ class MainActivity : AppCompatActivity() {
         }
 
         super.onBackPressed()
+    }
+
+    fun buildFilesMenu(filesDrawer: NavigationView){
+        val sideBarMenu = filesDrawer.menu
+        val filesListMenu = sideBarMenu.findItem(R.id.files_list).subMenu
+        val placeholderTest = FilesListItem("Testing test 123")
+        placeholderTest.renderToMenu(filesListMenu)
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        return true
+    }
+}
+
+class FilesListItem {
+    var id: Int
+    private var _type: String = "file"
+    private var rng = Random()
+    var type: String
+        get() {
+            return _type
+        }
+        set(newType:String) {
+            if (newType !== "file" || newType !== "folder"){
+                return
+            }
+            _type = newType
+        }
+    var name: String
+    var onClick = fun(){}
+
+    constructor(name:String) {
+        id = rng.nextInt()
+        this.name = name
+    }
+
+    fun renderToMenu(menu: Menu){
+        val menuSize = menu.size()
+        val menuItem = menu.add(
+            1,
+            id,
+            menuSize,
+            name
+        )
+
+        if (type === "folder"){
+            menuItem.setIcon(R.drawable.ic_folder)
+        }
+        else {
+            menuItem.setIcon(R.drawable.ic_file)
+        }
+    }
+
+    fun removeFromMenu(menu: Menu){
+        menu.removeItem(id)
     }
 }
