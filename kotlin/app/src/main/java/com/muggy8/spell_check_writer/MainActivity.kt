8e0a806d12c91962c103c801412610752a69d6e5
Zhi Kai Li <muggy8@gmail.com>
2023-01-06 01:19:31 -0800

    added toggling hamburger icon

--- kotlin/app/src/main/java/com/muggy8/spell_check_writer/MainActivity.kt
+++ kotlin/app/src/main/java/com/muggy8/spell_check_writer/MainActivity.kt
@@ -2,14 +2,44 @@ package com.muggy8.spell_check_writer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var primaryToolbar:Toolbar
+    private lateinit var filesDrawer:NavigationView
+    private lateinit var filesListToggle:ActionBarDrawerToggle
+    private lateinit var mainAppView: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var primaryToolbar:Toolbar = findViewById(R.id.primary_toolbar)
+        filesDrawer = findViewById(R.id.files_drawer_container)
+        mainAppView = findViewById(R.id.main_appview)
+        primaryToolbar = findViewById(R.id.primary_toolbar)
         setSupportActionBar(primaryToolbar)
+
+        filesListToggle = ActionBarDrawerToggle(
+            this,
+            mainAppView,
+            primaryToolbar,
+            R.string.open_files_drawer,
+            R.string.close_files_drawer,
+        )
+
+        mainAppView.addDrawerListener(filesListToggle)
+        filesListToggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        if (mainAppView.isDrawerOpen(GravityCompat.START)){
+            return mainAppView.closeDrawer(GravityCompat.START)
+        }
+
+        super.onBackPressed()
     }
 }
