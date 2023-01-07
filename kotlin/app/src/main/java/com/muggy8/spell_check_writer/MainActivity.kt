15efa49916690e01a8c495b260105d1a34f52dcc
Muggy Ate <muggy8@users.noreply.github.com>
2023-01-07 08:18:13 -0800

    added code for asking for permissions

--- kotlin/app/src/main/java/com/muggy8/spell_check_writer/MainActivity.kt
+++ kotlin/app/src/main/java/com/muggy8/spell_check_writer/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.muggy8.spell_check_writer
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.core.view.get
 import androidx.core.view.isEmpty
@@ -24,6 +33,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private lateinit var filesDrawer:NavigationView
     private lateinit var filesListToggle:ActionBarDrawerToggle
     private lateinit var mainAppView: DrawerLayout
+    private val permissionChecker: PermssionController = PermssionController(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,6 +83,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
         directoryListing.menuClicked(this, menuItem)
         return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        permissionChecker.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
 
@@ -185,5 +200,76 @@ class DirectoryList(pathName: String = "") {
                 return
             }
         }
+    }
+}
+
+class PermssionController (val context: AppCompatActivity) {
+    private val missingPermissions = mutableListOf<String>()
+    private val RECORD_REQUEST_CODE = 1337 // i have no idea what this is for but it seems like it should be constant
+
+    fun hasFileAccessPermission():Boolean{
+        var hasPermission = true
+        missingPermissions.clear()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val hasManageExternalStoragePermission = Environment.isExternalStorageManager()
+            hasPermission = hasPermission && hasManageExternalStoragePermission
+        }
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2) {
+            val hasReadPermission = ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+
+            if (!hasReadPermission){
+                missingPermissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+
+            hasPermission = hasPermission && hasReadPermission
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            val hasWritePermission = ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+
+            if (!hasWritePermission){
+                missingPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+
+            hasPermission = hasPermission && hasWritePermission
+        }
+
+        return hasPermission
+    }
+
+    fun requestFileAccessPermission(){
+        hasFileAccessPermission()
+        if (!missingPermissions.isEmpty()){
+            ActivityCompat.requestPermissions(
+                context,
+                missingPermissions.toTypedArray(),
+                RECORD_REQUEST_CODE,
+            )
+        }
+        else {
+            try {
+                val intentToGetManageStoragePermission = Intent(
+                    ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+                    Uri.parse("package:" + BuildConfig.APPLICATION_ID)
+                )
+                context.startActivity(intentToGetManageStoragePermission)
+            }
+            catch(e: Exception){
+                val intentToGetManageStoragePermission = Intent(ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                context.startActivity(intentToGetManageStoragePermission)
+            }
+        }
+    }
+
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
     }
 }
